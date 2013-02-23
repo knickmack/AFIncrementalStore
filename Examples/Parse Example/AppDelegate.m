4fba330271cb5676566407b392da7e9079aef2d2
Nik Macintosh <me@knickmack.com>
2013-02-22 23:50:16 -0800

    Add a managed object context, managed object model, and persistent store coordinator to the application delegate

--- Examples/Parse Example/AppDelegate.m
+++ Examples/Parse Example/AppDelegate.m
@@ -12,14 +12,20 @@
 @interface AppDelegate ()
 
 @property (strong, nonatomic, readonly) GameScoresViewController *gameScoresViewController;
+@property (strong, nonatomic, readonly) NSManagedObjectContext *managedObjectContext;
+@property (strong, nonatomic, readonly) NSManagedObjectModel *managedObjectModel;
 @property (strong, nonatomic, readonly) UINavigationController *navigationController;
+@property (strong, nonatomic, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
 @end
 
 @implementation AppDelegate
 
 @synthesize gameScoresViewController = _gameScoresViewController;
+@synthesize managedObjectContext = _managedObjectContext;
+@synthesize managedObjectModel = _managedObjectModel;
 @synthesize navigationController = _navigationController;
+@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
 
 #pragma mark - AppDelegate
 
@@ -41,12 +47,38 @@
     return _gameScoresViewController;
 }
 
+- (NSManagedObjectContext *)managedObjectContext {
+    if (!_managedObjectContext) {
+        _managedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+        
+        _managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator;
+    }
+    
+    return _managedObjectContext;
+}
+
+- (NSManagedObjectModel *)managedObjectModel {
+    if (!_managedObjectModel) {
+        _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"Parse" withExtension:@"momd"]];
+    }
+    
+    return _managedObjectModel;
+}
+
 - (UINavigationController *)navigationController {
     if (!_navigationController) {
         _navigationController = [[UINavigationController alloc] initWithRootViewController:self.gameScoresViewController];
     }
     
     return _navigationController;
+}
+
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
+    if (!_persistentStoreCoordinator) {
+        _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:self.managedObjectModel];
+    }
+    
+    return _persistentStoreCoordinator;
 }
 
 #pragma mark - UIApplicationDelegate
